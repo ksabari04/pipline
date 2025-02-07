944653ebf8495aa500b271618cc13a390d6495c8
ksabari04 <147916315+ksabari04@users.noreply.github.com>
2025-02-07 12:50:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $serverStatus = 'online'; // You can use PHP to check server-related data
 
 // Display appropriate message based on server status
 if ($serverStatus == 'online') {
-    $statusMessage = "Server is running smoothly!!!!!!!!!!!!!";
+    $statusMessage = "Server is running smoothly";
     $statusClass = "online";
 } else {
     $statusMessage = "Server is down, please try again later.";
@@ -22,7 +22,7 @@ if ($serverStatus == 'online') {
 </head>
 <body>
     <div class="container">
-        <h1>Welcome to my php server om sai</h1>
+        <h1>Welcome to my php server</h1>
         <div class="status-box <?php echo $statusClass; ?>">
             <p class="status-message"><?php echo $statusMessage; ?></p>
             <button class="refresh-btn" onclick="window.location.reload();">Check Again</button>
